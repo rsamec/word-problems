e4a6024aa0ffe86715127e988a47f7d7b3e316d7
rsamec <roman.samec2@gmail.com>
2025-03-08 21:18:56 +0100

    add source map

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const config = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
-  devtool: 'source-map',
+  devtool: 'eval-cheap-module-source-map',
   // Enable webpack-dev-server to get hot refresh of the app.
   devServer: {
     static: './build',
